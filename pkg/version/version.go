baad445909db290117208beecc036e36ef699ad6
Vova Nekhai <vova.nehai@anodot.com>
2020-01-22 23:26:08 +0200

    set version to 2.2.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 //to update version, just set variable with new version and run `make version-set`
-var VERSION = "2.2.0-alpha2"
+var VERSION = "2.2.0"
 
 //set during build
 var REVISION = "unknown"

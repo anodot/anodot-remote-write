6523ab4b8bdd92301c34cf76f2fac08527f74f38
Vova Nekhai <vova.nehai@anodot.com>
2020-01-23 15:28:52 +0200

    fix test

--- pkg/relabling/pod_test.go
+++ pkg/relabling/pod_test.go
@@ -3,23 +3,24 @@ package relabling
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestMarshar(t *testing.T) {
+func TestJsonMarshal(t *testing.T) {
 	includedPods := NewCache()
 	excluded := NewCache()
 
 	excluded.Store(SaveEntry{
-		Name:      "exluded",
-		Labels:    map[string]string{"1": "2"},
-		Namespace: "kube-system",
+		Name:        "exluded",
+		ChangedName: "name-1",
+		Namespace:   "kube-system",
 	})
 
 	includedPods.Store(SaveEntry{
-		Name:      "included",
-		Labels:    map[string]string{"1": "2"},
-		Namespace: "kube-system",
+		Name:        "included",
+		ChangedName: "name-2",
+		Namespace:   "kube-system",
 	})
 	mapping := PodsMapping{
 		WhitelistedPods: includedPods,
@@ -31,5 +32,8 @@ func TestMarshar(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 
-	fmt.Println(string(marshal))
+	s := `{"WhitelistedPods":{"Data":{"kube-system|included":"name-2"}},"ExcludedPods":{"Data":{"kube-system|exluded":"name-1"}}}`
+	if !reflect.DeepEqual(marshal, []byte(s)) {
+		t.Fatal(fmt.Sprintf("Wrong json for PodsMapping \n got: %s\n want: %s", string(marshal), s))
+	}
 }

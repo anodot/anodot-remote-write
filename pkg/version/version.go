10bb4b98c59d1b0745ea221b1c45bd22587c2c93
Vova Nekhai <vova.nehai@anodot.com>
2020-04-14 22:16:12 +0300

    Fix infinite loop

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 //to update version, just set variable with new version and run `make version-set`
-var VERSION = "2.2.2"
+var VERSION = "2.2.3"
 
 //set during build
 var REVISION = "unknown"

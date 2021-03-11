8f3d426889af56fa94b019238c2b2024a0d7ddb7
PavelBerdyuhin <pberdyuhin@anodot.com>
2021-03-11 12:28:57 +0200

    Update application version to 2.4.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 //to update version, just set variable with new version and run `make version-set`
-var VERSION = "2.3.2"
+var VERSION = "2.4.0"
 
 //set during build
 var REVISION = "unknown"
